9eb711ecfbddaaef8ab06a5ea4dbc677263fc4a3
DS <aboussoubel@gmail.com>
2025-06-11 14:57:40 +0700

    Implement getComputerChoice and getHumanChoice
    The getHumanChoice function prompts the client into making a choice. In
    the other hand, a computer would respond randomly.

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello World")
+const rps = ['rock', 'paper', 'scissors'];
+
+// Returns a random integer between 0 and max (excluded)
+let getRandomInt = max => Math.floor(Math.random() * max);
+
+function getComputerChoice() {
+    return rps[getRandomInt(rps.length)];
+}
+
+function getHumanChoice() {
+    let choice = prompt("What are you playing?");
+
+    if( rps.includes(choice) )
+        return choice;
+
+    console.log("Invalide choice, have at it again!");
+        
+    // Try again    
+    return getHumanChoice();
+}
+
+console.log(getHumanChoice());
